05a25aefa48b3d900ba80f89c4aa7a00aecb33c5
Kerkkoh <superihippo@gmail.com>
2019-11-06 20:11:34 +0200

    Build kansio

---
+++ build/precache-manifest.25215f31fe305b9a8f7011f750195e9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f919ac8f5073a0428c42d1e3bea0978",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5daf4ee9c2fa896607b",
+    "url": "/static/css/main.3ac58117.chunk.css"
+  },
+  {
+    "revision": "1c065c49533ebf68fbe0",
+    "url": "/static/js/2.38d5554f.chunk.js"
+  },
+  {
+    "revision": "a5daf4ee9c2fa896607b",
+    "url": "/static/js/main.25397aee.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
